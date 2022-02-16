d8cbd776f1306fe7e7f56ec0aff4e03371e52f1f
Nadia Andreichuck <nadika.alv@gmail.com>
2022-02-16 22:01:49 +0200

    feat: add function

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ Write your code in `src/index.js.
 2. Fork this repository: human-readable-number
 3. Clone your newly created repo: https://github.com/<%your_github_username%>/human-readable-number/  
 4. Go to folder `human-readable-number`  
-5. To install all dependencies use [`npm install`](https://docs.npmjs.com/cli/install)  
+5. To install all dependencies use   [`npm install
 6. Run `npm test` in the command line  
 7. You will see the number of passing and failing tests you 100% of passing tests is equal to 100p in score  
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    let length = String(number).length;
+    
+    const digits = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const secondDigits = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const dozens = [null, 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    switch (length) {
+        case 1:
+            return digits[number];
+        case 2:
+            if (number % 10 === 0) {
+                return dozens[Math.floor(number / 10)]
+            } else if (Math.floor(number / 10) === 1) {
+                return secondDigits[number % 10]
+            } else {
+                return `${dozens[Math.floor(number / 10)]} ${digits[number % 10]}`;
+            }
+    
+        case 3:
+            if (number % 100 === 0) {
+                return `${digits[Math.floor(number / 100)]} hundred`
+            } else if (Math.floor(number % 100 / 10) === 0) {
+                return `${digits[Math.floor(number / 100)]} hundred ${digits[number % 10]}`
+            } else if (number % 10 === 0) {
+                return `${digits[Math.floor(number / 100)]} hundred ${dozens[Math.floor((number % 100) / 10)]}`
+            } else if (Math.floor((number % 100) / 10) === 1) {
+                return `${digits[Math.floor(number / 100)]} hundred ${secondDigits[number % 10]}`
+            } else {
+                return `${digits[Math.floor(number / 100)]} hundred ${dozens[Math.floor((number % 100) / 10)]} ${digits[number % 10]}` 
+            }
+           
+    }
+}      
+      
